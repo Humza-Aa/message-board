b8d6625321cb5a06cf2831247290937ee27dc172
Humza-A <humzaaamir61@gmail.com>
2022-08-28 15:17:13 -0400

    added simple routing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+   {
+     text: "Hi there!",
+     user: "Amando",
+     added: new Date()
+   },
+   {
+     text: "Hello World!",
+     user: "Charles",
+     added: new Date()
+   },
+   {
+    text: "Hello!",
+    user: "Darwin",
+    added: new Date()
+   },
+   {
+    text: "BJY!",
+    user: "Enstien",
+    added: new Date()
+   }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Mini Messageboard", messages: messages });
 });
+
+router.get('/new', function(req, res, next) {
+  res.render('form');
+});
+
+router.post('/new', function (req, res, next) {
+  console.log(req.body.message);
+  messages.push({text: req.body.message, user: req.body.author, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
